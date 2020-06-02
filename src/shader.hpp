7cfe9b8642fe69e207ebd56f362b6358f91cf20b
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-06-02 09:59:45 +0200

    Add shader class

---
+++ src/shader.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace open_pokemon_tcg {
+
+  class Shader {
+  public:
+    Shader(std::string vertex_shader_path, std::string fragment_shader_path);
+    ~Shader();
+
+    // Mutators
+    void use();
+    void set_uniform(const GLchar* uniform, const GLfloat *data);
+
+  private:
+    GLuint program;
+    GLuint vertex_shader;
+    GLuint fragment_shader;
+
+    // Accessors
+    GLuint load_shader_file(std::string shader_path, GLenum shaderType);
+  };
+}
