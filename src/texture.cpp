b8263205861905722ccd076979479ec0f4747e86
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-06-12 08:20:54 +0200

    Fix card flickering

--- src/texture.cpp
+++ src/texture.cpp
@@ -30,8 +30,8 @@ Texture::Texture(std::string img_path) {
 
   // Texture filtering
   glGenerateMipmap(GL_TEXTURE_2D);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
   // Max samples (EXT stands for extension, and thus not from OpenGL specification)
   float anisotropy = 16.0f;
