0d018ab058c11540fd11c4d78483ecc6fece794d
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-06-25 12:02:50 +0200

    Add attack model

---
+++ src/game/model/attack.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "energy.hpp"
+#include "effects/attack_effect.hpp"
+
+#include <string>
+
+namespace open_pokemon_tcg::game::model {
+
+  class Attack {
+  public:
+    Attack();
+    Attack(std::string name, unsigned int damage, EnergyAmount cost);
+    Attack(std::string name, unsigned int damage, EnergyAmount cost, effects::IAttackEffect &effect);
+    ~Attack();
+
+    // Mutators
+    void perform(effects::IHealthTarget &target);
+
+    // Accessors
+    std::string name() const;
+    unsigned int damage() const;
+    EnergyAmount cost() const;
+
+  private:
+    std::string _name;
+    unsigned int _damage;
+    EnergyAmount _cost;
+    effects::IAttackEffect *_effect;
+  };
+
+}
---
+++ src/game/model/effects/attack_effect.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "effect_target.hpp"
+
+namespace open_pokemon_tcg::game::model::effects {
+
+  class IAttackEffect {
+  public:
+    IAttackEffect() {}
+    virtual ~IAttackEffect() = 0;
+
+    // Mutators
+    virtual bool perform(unsigned int &damage) = 0; // returns false if the attack damage should be canceled
+  };
+
+}
