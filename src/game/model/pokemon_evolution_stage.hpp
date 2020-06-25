60fcfb8724428cb52ea3213e1ab77cdef3623d7b
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-06-25 12:00:13 +0200

    Add evolution stages

---
+++ src/game/model/pokemon_evolution_stage.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace open_pokemon_tcg::game::model {
+
+  enum EvolutionStage {
+    BASIC,
+    STAGE1,
+    STAGE2,
+    EX,
+    MEGA_EX,
+    RESTORED,
+    BREAK,
+  };
+
+}
