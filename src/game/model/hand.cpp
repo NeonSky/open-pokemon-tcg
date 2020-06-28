76e1a7cb2bd196ca2f0f62cae3218cdef56fd659
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-06-28 11:00:10 +0200

    Add hand model

---
+++ src/game/model/hand.cpp
@@ -0,0 +1,39 @@
+#include "hand.hpp"
+
+#include "../../engine/debug/logger.hpp"
+
+using namespace open_pokemon_tcg::game::model;
+
+Hand::Hand() = default;
+Hand::Hand(std::vector<std::reference_wrapper<ICard>> cards) : _cards(cards) {}
+Hand::~Hand() = default;
+
+void Hand::add(ICard& card) {
+  _cards.push_back(card);
+  _on_add(card);
+}
+
+void Hand::remove(int index) {
+  _cards.erase(_cards.begin() + index);
+  _on_remove(index);
+}
+
+void Hand::listen_on_add(std::function<void (ICard& card)> callback) {
+  _on_add.append(callback);
+}
+
+void Hand::listen_on_remove(std::function<void (int index)> callback) {
+  _on_remove.append(callback);
+}
+
+unsigned int Hand::size() const {return _cards.size(); }
+
+const std::vector<std::reference_wrapper<ICard>>& Hand::cards() const { return _cards; }
+
+unsigned int Hand::find(ICard &card) const {
+  for (unsigned int i = 0; i < _cards.size(); i++)
+    if (&_cards[i].get() == &card)
+      return i;
+
+  LOG_ERROR("Could not find card in hand: " + card.debug());
+}
---
+++ src/game/model/hand.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "card.hpp"
+
+#include "../../engine/event/event.hpp"
+
+#include <functional>
+#include <vector>
+
+namespace open_pokemon_tcg::game::model {
+
+  class Hand {
+  public:
+    Hand();
+    Hand(std::vector<std::reference_wrapper<ICard>> cards);
+    ~Hand();
+
+    // Mutators
+    void add(ICard& card);
+    void remove(int index);
+    void listen_on_add(std::function<void (ICard& card)> callback);
+    void listen_on_remove(std::function<void (int index)> callback);
+
+    // Accessors
+    unsigned int size() const;
+    const std::vector<std::reference_wrapper<ICard>>& cards() const;
+    unsigned int find(ICard &card) const;
+
+  private:
+    std::vector<std::reference_wrapper<ICard>> _cards;
+
+    engine::event::CallbackList<void (ICard& card)> _on_add;
+    engine::event::CallbackList<void (int index)> _on_remove;
+  };
+}
