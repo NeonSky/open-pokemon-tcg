9ae39d531db26c5e3beff4be1a21c6d167adce43
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-07-03 10:35:27 +0200

    Operate player controls from gui

--- src/game/scenes/duel.hpp
+++ src/game/scenes/duel.hpp
@@ -51,7 +51,7 @@ namespace open_pokemon_tcg::game::scenes {
 
     view::Game *_game;
 
-    view::Card *selected_card;
+    view::Card *_selected_card;
     engine::graphics::Rectangle *debug_rect;
     bool focus_hovered_card = false;
 
@@ -61,7 +61,7 @@ namespace open_pokemon_tcg::game::scenes {
   };
 
   Duel::Duel(engine::gui::Window* window) : camera(DebugCamera(window, this->camera1_transform)) {
-    this->selected_card = nullptr;
+    _selected_card = nullptr;
 
     window->add_on_key_callback(std::bind(&Duel::on_key, this, std::placeholders::_1));
     window->add_on_mouse_click_callback(std::bind(&Duel::on_mouse_click, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
@@ -81,6 +81,7 @@ namespace open_pokemon_tcg::game::scenes {
     };
     LOG_DEBUG("Decks loaded.");
 
+    // TODO: Investigate why this takes so long
     std::array<std::string, 2> names {"Alice", "Bob"};
     std::shared_ptr<model::Game> game_model = std::make_shared<model::Game>(decks, names);
     _game = new view::Game(game_model, *this->playmat);
@@ -165,32 +166,56 @@ namespace open_pokemon_tcg::game::scenes {
     this->debug_rect->render(view_projection);
   }
 
-  void Duel::gui() {}
+  void Duel::gui() {
+    ImGui::Begin("Statistics");
+    ImGui::Text("Turn: %d", _game->model().turn());
+    ImGui::Text("Player: %s", _game->model().current_player().name().c_str());
+    ImGui::End();
+
+    ImGui::Begin("Player Controls");
+
+    if (ImGui::Button("End Turn")) {
+      _game->model().end_turn();
+      this->current_player = (this->current_player == this->player1) ? this->player2 : player1; // TODO: Maybe move to view?
+      this->camera.set_transform((this->current_player == this->player1) ? this->camera1_transform : this->camera2_transform);
+
+      // if (_game->model().winner() != nullptr) {
+      //   LOG_DEBUG("We have a winner!");
+      //   LOG_DEBUG("The winner is: " + _game->model().winner()->name());
+      //   return;
+      // }
+    }
+
+    if (_selected_card != nullptr) {
+      ImGui::Separator();
+
+      ImGui::Text("Selected Card Options");
+      if (ImGui::Button("Place on active slot")) {
+        _game->model().current_player().active_pokemon_from_hand(_selected_card->_model);
+        _selected_card = nullptr;
+      }
+      if (ImGui::Button("Place on bench")) {
+        _game->model().current_player().bench_pokemon_from_hand(_selected_card->_model);
+        _selected_card = nullptr;
+      }
+
+      ImGui::Separator();
+    }
+    ImGui::End();
+
+    ImGui::Begin("Debug Controls");
+    if (ImGui::Button("Draw Card"))
+      _game->model().current_player().draw();
+    if (ImGui::Button("Mill Card"))
+      _game->model().current_player().mill();
+    if (ImGui::Button("Take Prize Card"))
+      _game->model().current_player().take_prize_card();
+    ImGui::End();
+  }
 
   void Duel::on_key(GLFWwindow* window) {
-    // Mill
-    if (glfwGetKey(window, GLFW_KEY_M))
-      _game->model().current_player().mill();
-
-    // Hand
-    if (glfwGetKey(window, GLFW_KEY_H))
-      _game->model().current_player().draw();
-
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
-      this->selected_card = nullptr;
-    }
-
-    // Take prize card
-    if (glfwGetKey(window, GLFW_KEY_L))
-      _game->model().current_player().take_prize_card();
-
-    // Bench
-    if (glfwGetKey(window, GLFW_KEY_B)) {
-      if (this->selected_card != nullptr) {
-        _game->model().current_player().bench_pokemon_from_hand(this->selected_card->_model);
-        this->selected_card = nullptr;
-      }
-    }
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+      _selected_card = nullptr;
 
     // Switch which user/player to control
     if (glfwGetKey(window, GLFW_KEY_U)) {
@@ -218,14 +243,14 @@ namespace open_pokemon_tcg::game::scenes {
 
       view::IPlaymat::Side current_side = (this->current_player == this->player1) ? view::IPlaymat::Side::PLAYER1 : view::IPlaymat::Side::PLAYER2;
 
-      if (this->selected_card != nullptr) {
+      if (_selected_card != nullptr) {
         if (engine::geometry::ray_rectangle_intersection(ray, this->playmat->active_slot(current_side))) {
-          _game->model().current_player().active_pokemon_from_hand(this->selected_card->_model);
-          this->selected_card = nullptr;
+          _game->model().current_player().active_pokemon_from_hand(_selected_card->_model);
+          _selected_card = nullptr;
         }
       }
 
-      this->selected_card = nullptr;
+      // _selected_card = nullptr;
       float best_dist = 1e9;
 
       for (auto &card : this->current_player->hand->cards()) {
@@ -234,7 +259,7 @@ namespace open_pokemon_tcg::game::scenes {
           float dist = glm::distance(hit->point, this->camera.transform().position);
           if (dist < best_dist) {
             best_dist = dist;
-            this->selected_card = card;
+            _selected_card = card;
           }
         }
       }
