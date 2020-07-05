e522e2a3e52fb002e75061fcf0a46fc4540a1a44
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-07-05 16:29:04 +0200

    Add specific pokemon card view

---
+++ src/game/view/pokemon_card.cpp
@@ -0,0 +1,50 @@
+#include "pokemon_card.hpp"
+
+using namespace open_pokemon_tcg;
+using namespace open_pokemon_tcg::game::view;
+
+PokemonCard::PokemonCard(game::model::PokemonCard &model, engine::geometry::Transform transform)
+  : _model(model),
+    _pokemon_view(*new Card(model, transform)),
+    transform(transform) {
+
+  for (auto& e : _model.attached_energy())
+    _energy_views.push_back(new Card(e, transform));
+
+  update_transforms();
+
+  _model.on_energy_attached([this](const model::BasicEnergy& energy_card) {
+    _energy_views.push_back(new Card(energy_card, this->transform));
+    update_transforms();
+  });
+
+  _model.on_energy_detached([this](unsigned int index) {
+    delete _energy_views[index];
+    _energy_views.erase(_energy_views.begin() + index);
+    update_transforms();
+  });
+}
+
+PokemonCard::~PokemonCard() = default;
+
+void PokemonCard::render(const glm::mat4 &view_projection_matrix, engine::graphics::Shader *shader) {
+  _pokemon_view.render(view_projection_matrix, shader);
+  for (auto& e : _energy_views)
+    e->render(view_projection_matrix, shader);
+}
+
+engine::geometry::Intersection* PokemonCard::does_intersect(engine::geometry::Ray ray) const {
+  return _pokemon_view.does_intersect(ray);
+}
+
+const engine::graphics::Texture& PokemonCard::texture() const {
+  return _pokemon_view.texture();
+}
+
+// Privates
+void PokemonCard::update_transforms() {
+  glm::vec3 offset = 0.1f * (-transform.right() + -transform.up()) + (-0.001f * transform.forward());
+  int i = 1;
+  for (auto& e : _energy_views)
+    e->transform.position = transform.position + float(i++) * offset;
+}
---
+++ src/game/view/pokemon_card.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "card.hpp"
+#include "../model/card/pokemon.hpp"
+
+namespace open_pokemon_tcg::game::view {
+  class PokemonCard {
+  public:
+    PokemonCard(game::model::PokemonCard &model, engine::geometry::Transform transform);
+    ~PokemonCard();
+
+    // Mutators
+    void render(const glm::mat4 &view_projection_matrix, engine::graphics::Shader *shader);
+
+    // Accessors
+    glm::mat4 model_matrix() const;
+    engine::geometry::Intersection* does_intersect(engine::geometry::Ray ray) const;
+    const engine::graphics::Texture& texture() const;
+
+  private:
+    game::model::PokemonCard& _model;
+    Card& _pokemon_view;
+    std::vector<Card*> _energy_views;
+
+    void update_transforms();
+
+  public:
+    engine::geometry::Transform transform; // FIXME
+
+  };
+}
